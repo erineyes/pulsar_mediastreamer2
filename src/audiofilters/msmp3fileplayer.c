362e32366121830d0a076e658fdd9834afb8eb27
Tae-kyu, Han <nemesis.han@gmail.com>
2024-09-26 00:28:35 +0900

    Update msmp3fileplayer.c

--- src/audiofilters/msmp3fileplayer.c
+++ src/audiofilters/msmp3fileplayer.c
@@ -168,7 +168,7 @@ static int mp3_player_open(MSFilter *f, void *arg) {
 		mpg123_param(d->mpg123, MPG123_RESYNC_LIMIT, -1, 0);
 		mpg123_format_none(d->mpg123);
 		//mpg123_format(d->mpg123, MPG123_ANY, MPG123_MONO | MPG123_STEREO, MPG123_ENC_SIGNED_8 | MPG123_ENC_UNSIGNED_8 | MPG123_ENC_SIGNED_16 | MPG123_ENC_UNSIGNED_16 | MPG123_ENC_FLOAT_32);
-		mpg123_format(d->mpg123, 44100, MPG123_MONO | MPG123_STEREO, MPG123_ENC_SIGNED_16);
+		mpg123_format(d->mpg123, 44100, MPG123_MONO | MPG123_STEREO, MPG123_ENC_SIGNED_16 | MPG123_ENC_UNSIGNED_16);
 		//mpg123_param(d->mpg123, MPG123_ADD_FLAGS, MPG123_ENC_SIGNED_16, 0);  		
 		//mpg123_format_all(d->mpg123);  
 
@@ -179,8 +179,8 @@ static int mp3_player_open(MSFilter *f, void *arg) {
 			d->is_mp3 = 1;
 			int encoding;
 			mpg123_getformat(d->mpg123, &d->rate, &d->nchannels, &encoding);			
-			d->samplesize = mpg123_encsize(encoding) / d->nchannels;
-			//d->samplesize = 2;
+			//d->samplesize = mpg123_encsize(encoding) / d->nchannels;
+			d->samplesize = 2;
 			d->hsize = 0;
 			d->is_raw = FALSE; 
 		}
